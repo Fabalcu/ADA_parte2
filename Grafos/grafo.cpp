986160f0eccc036c2a0f2d2b9b4ad6ef83da0bca
AngelJSD <angel.sucapuca@ucsp.edu.pe>
2016-06-08 23:29:34 -0500

    Grafo

---
+++ Grafos/grafo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define vec vector<vector<pair<int,int> > >
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vec graph(n);
+    vector<int> pesos(m);
+
+    int a,b,p;
+    for(int i=0;i<m;++i)
+    {
+        cin>>a>>b>>p;
+        graph[a].push_back(make_pair(b,p));
+    }
+
+    for(int i=0; i<n; ++i){
+
+        cout<<"nodo: "<<i<<endl;
+        for(int j=0; j<graph[i].size(); ++j){
+
+            cout<<"a nodo "<<graph[i][j].first<<" con peso: "<<graph[i][j].second<<endl;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
